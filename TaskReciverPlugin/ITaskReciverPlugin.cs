0cedd4abb669d854c15d50728cd2e12918aba2ce
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-10-14 17:41:22 +0200

    Plugininterface implementation

---
+++ TaskReciverPlugin/ITaskReciverPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskReceiver.Plugin
+{
+    public interface ITaskReciverPlugin
+    {
+        string PluginName { get;  }
+        string CommandTrigger { get; }
+
+        void Execute(List<Tuple<string, string>> param);
+    }
+}
---
+++ TaskReciverPlugin/PluginLoader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TaskReceiver.Plugin
+{
+    public class PluginLoader
+    {
+        private string[] dllFileNames = null;
+        public ICollection<ITaskReciverPlugin> LoadedPlugins { get; private set; }
+
+        public PluginLoader(string pluginDir)
+        {
+            if (Directory.Exists(pluginDir))
+            {
+                dllFileNames = Directory.GetFiles(pluginDir, "*.dll");
+            }
+            else
+            {
+                Directory.CreateDirectory(pluginDir);
+            }
+        }
+
+        public void LoadAll()
+        {
+            LoadedPlugins = new List<ITaskReciverPlugin>();
+
+            foreach (string dllFile in dllFileNames)
+            {
+                AssemblyName an = AssemblyName.GetAssemblyName(dllFile);
+                Assembly assembly = Assembly.Load(an);
+
+                Type pluginType = typeof(ITaskReciverPlugin);
+
+                if (assembly != null)
+                {
+                    Type[] types = assembly.GetTypes();
+                    foreach (Type type in types)
+                    {
+                        if (type.IsInterface || type.IsAbstract)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            if (type.GetInterface(pluginType.FullName) != null)
+                            {
+                                ITaskReciverPlugin plugin = (ITaskReciverPlugin)Activator.CreateInstance(type);
+                                LoadedPlugins.Add(plugin); 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
