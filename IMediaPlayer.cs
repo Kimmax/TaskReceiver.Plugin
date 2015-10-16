6419abc3d5c6b7ca0e631f0c8d0b63d044d65331
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-10-16 13:36:59 +0200

    plugin interface implementation

---
+++ IMediaPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskReceiver.Plugin
+{
+    public interface IMediaPlayer : ITaskReciverPlugin
+    {
+        void PlayPause();
+        void Stop();
+        void IncreaseVolume();
+        void DecreaseVolume();
+        void SetVolume(int volume);
+        void NextTitle();
+        void PreviouseTitle();
+    }
+}
